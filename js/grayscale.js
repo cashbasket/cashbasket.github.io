7a8089922a471ca907d783c17e7588ea03c1c480
Ryan Machan <rjm21@case.edu>
2018-02-07 22:35:44 -0500

    update navbar brand styles and behavior

--- js/grayscale.js
+++ js/grayscale.js
@@ -7,9 +7,9 @@
 // jQuery to collapse the navbar on scroll
 function collapseNavbar() {
 	if ($('.navbar').offset().top > 50) {
-		$('.navbar-fixed-top').addClass('top-nav-collapse');
+		$('.navbar-fixed-top, .navbar-brand').addClass('top-nav-collapse');
 	} else {
-		$('.navbar-fixed-top').removeClass('top-nav-collapse');
+		$('.navbar-fixed-top, .navbar-brand').removeClass('top-nav-collapse');
 	}
 }
 
